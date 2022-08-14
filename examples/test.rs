090bd79f71790746ceb25f298cd550b7e5ad3eec
Fred Morcos <fm@fredmorcos.com>
2022-08-14 21:40:17 +0200

    Add tests for functions with 2+ arguments

--- examples/test.rs
+++ examples/test.rs
@@ -7,6 +7,25 @@ pub(crate) fn run_10_times_per_second(msg: &str) -> &'static str {
   "foo"
 }
 
+#[throttle(10, Duration::from_secs(1))]
+pub(crate) fn run_10_times_per_second_with_2_args(
+  msg1: &str,
+  msg2: &str,
+) -> &'static str {
+  eprintln!("msg1={}, msg2={}", msg1, msg2);
+  "foo2"
+}
+
+#[throttle(10, Duration::from_secs(1))]
+pub(crate) fn run_10_times_per_second_with_3_args(
+  msg1: &str,
+  msg2: &str,
+  msg3: &str,
+) -> &'static str {
+  eprintln!("msg1={}, msg2={}, msg3={}", msg1, msg2, msg3);
+  "foo2"
+}
+
 #[throttle(1, Duration::from_millis(100))]
 pub(crate) fn run_once_per_100_milliseconds(msg: &str) -> &'static str {
   eprintln!("{}", msg);
@@ -17,6 +36,31 @@ fn main() {
   for _ in 0..3 {
     for i in 0..20 {
       let _ = run_10_times_per_second(&format!("{}: Running 10 times per second", i));
+    }
+
+    eprintln!();
+    std::thread::sleep(Duration::from_secs(1));
+  }
+
+  for _ in 0..3 {
+    for i in 0..20 {
+      let _ = run_10_times_per_second_with_2_args(
+        &format!("{}: Running 10 times per second with 2 args", i),
+        "other message",
+      );
+    }
+
+    eprintln!();
+    std::thread::sleep(Duration::from_secs(1));
+  }
+
+  for _ in 0..3 {
+    for i in 0..20 {
+      let _ = run_10_times_per_second_with_3_args(
+        &format!("{}: Running 10 times per second with 3 args", i),
+        "other message",
+        "another message",
+      );
     }
 
     eprintln!();
@@ -43,6 +87,37 @@ fn main() {
           "{}: Running 10 times per second from thread {:?}",
           i, t
         ));
+      });
+    }
+
+    eprintln!();
+    std::thread::sleep(Duration::from_secs(1));
+  }
+
+  for _ in 0..3 {
+    for i in 0..20 {
+      thread::spawn(move || {
+        let t = thread::current().id();
+        let _ = run_10_times_per_second_with_2_args(
+          &format!("{}: Running 10 times per second with 2 args from thread {:?}", i, t),
+          "other message",
+        );
+      });
+    }
+
+    eprintln!();
+    std::thread::sleep(Duration::from_secs(1));
+  }
+
+  for _ in 0..3 {
+    for i in 0..20 {
+      thread::spawn(move || {
+        let t = thread::current().id();
+        let _ = run_10_times_per_second_with_3_args(
+          &format!("{}: Running 10 times per second with 3 args from thread {:?}", i, t),
+          "other message",
+          "another message",
+        );
       });
     }
 
